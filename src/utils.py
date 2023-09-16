d3f266f254d64165e914230ebca88ad522f43b37
Mack Shonayi <shonayimack@gmail.com>
2023-09-16 02:08:46 +0200

    Complete Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ def evaluate_models(X_train,y_train,X_test,y_test, models,params):
 
             gs = GridSearchCV(model, param, cv=3)
             gs.fit(X_train, y_train) # Train Model
-            
+
             model.set_params(**gs.best_params_)
             model.fit(X_train, y_train) # Train Model
 
@@ -42,5 +42,12 @@ def evaluate_models(X_train,y_train,X_test,y_test, models,params):
 
             report[list(models.keys())[i]] = test_model_score 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
